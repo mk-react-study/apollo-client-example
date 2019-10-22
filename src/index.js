ad8bdb3f07af87aa5753f2135578c1a9ca1e2e8c
Mohit Kumar <mkgithub@gmail.com>
2019-10-21 21:42:20 -0400

    ApolloHooksProvider

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 import apolloClient from './lib/apollo-client'
-
-import { ApolloProvider } from '@apollo/react-hooks'
+import { ApolloProvider } from 'react-apollo' // To use with react-apollo's ApolloProvider already present in your project:
+import { ApolloProvider as ApolloHooksProvider } from '@apollo/react-hooks'
+//https://www.npmjs.com/package/react-apollo-hooks
 
 const WithApollo = () => (
   <ApolloProvider client={apolloClient}>
-    <App />
+    <ApolloHooksProvider client={apolloClient}>
+      <App />
+    </ApolloHooksProvider>
   </ApolloProvider>
 )
 
